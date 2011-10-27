fda7dd631c684f26754a8124ce9219d819c2a5bc
Burt Holzman <burt@fnal.gov>
2011-10-27 16:55:02 -0500

    Added more figures

--- processing.sh
+++ processing.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if ((0)); then
+arrow_head="path 'M 0,0 l -30,-10 +10,+10 -10,+10 +30,-10 z'"
+pointer="$arrow_head stroke-width 3 line 0,0, -100,0 "
+
+if ((1)); then
 # figure 1 -- fnal history parser
 convert FNALViewHistoryParser2.jpg -pointsize 36 -crop 720x480+0+25 -draw "text 90, 415 'Goal = 98%, Achieved = 99.6%'" processed/Overall_Hourly_Availability.jpg
 
@@ -9,7 +12,7 @@ cp t1-performance-tests.jpg processed
 
 
 # figure 3 - site availability
-arrow_head="path 'M 0,0 l -30,-10 +10,+10 -10,+10 +30,-10 z'"
+
 convert site-avail.png -extent 900x620 -pointsize 36 \
     -fill white -stroke white \
     -draw "rectangle 0,500 800,620" \
@@ -35,7 +38,6 @@ convert site-avail.png -extent 900x620 -pointsize 36 \
 
 # figure 4 - job robot rank
 
-arrow_head="path 'M 0,0 l -30,-10 +10,+10 -10,+10 +30,-10 z'"
 convert month-ranking-plot.png  -pointsize 36 -extent 900x500 \
     -fill white -stroke white \
     -draw "rectangle 0,388 800,500" \
@@ -79,7 +81,7 @@ convert -extent 720x604+0-100 tape_verified_delay_plot-90.jpg \
 \
     processed/tape-latency.png
 
-fi
+
 # figure 6 - computing utilization
 convert cms_farm_usgae.jpg \
     -fill lightblue -stroke lightblue \
@@ -93,4 +95,69 @@ convert cms_farm_usgae1.jpg \
 
 
 
+# figure 8a - processing share
+convert processing-share.png \
+    -fill \#ae8283 -stroke \#ae8283 \
+    -pointsize 24 \
+    -draw "translate 400,100 text 0, 0 'FNAL' translate -80,70 rotate 135 $pointer " \
+    processed/processing-share.png
+
+
+# figure 8b - percent utilization wrt share
+convert utilization_pledged.png \
+    -fill green -stroke green \
+    -pointsize 18 \
+    -draw "translate 400,350 text 0, 0 'FNAL' translate -80,70 rotate 135 $pointer " \
+    -strokewidth 3 \
+    -fill tan -stroke tan \
+    -draw "translate 0, 516 line 100,0 718,0" \
+    -strokewidth 1 \
+    -draw "translate 550,440 text 0, 0 '100% utilization' translate -80,70 rotate 135 $pointer " \
+	processed/utilization_pledged.png
+
+
+# figure 9 - successful and failed jobs
+convert siteActivity_individual.png \
+    processed/success_failed_jobs.png
+
+#figure 10 - dcache data rates
+convert cms_dcache_transfers.jpg \
+    processed/dcache_data_rates.png
+
+
+#figure 11 - bytes on tape
+convert -extent 740x504 -crop 740x480+0-24 cms_bytes2.jpg \
+    -fill white -stroke white \
+    -draw "rectangle 70,401 720,504" \
+    -fill black -stroke black \
+    -pointsize 28 \
+    -draw "translate 70,430 \
+    text 0,0 '7/1' \
+    text 200,0 '8/1' \
+    text 400,0 '9/1' \
+    text 600,0 '9/30'" \
+\
+    -pointsize 24 \
+    -draw "fill yellow stroke white text 200, 150 'Deleted/Squeezable'" \
+    -draw "fill yellow stroke yellow text 200, 270 'Monte Carlo'" \
+    -draw "text 200, 350 'Custodial Data'" \
+    -pointsize 18 \
+    -draw "text 200, 392 ' Legacy'" \
+    -draw "fill magenta stroke magenta translate 500,255 text 0, 0 'Users'  scale .5,.5 translate 190,-80 rotate -30 $pointer " \
+    processed/bytes_on_tape.png
+
+fi
+# figure 12
+convert -extent 740x504 -crop 740x480+0-24 cms_bytes3.jpg \
+    -fill white -stroke white \
+    -draw "translate 0,20 rectangle 70,401 720,504" \
+    -fill black -stroke black \
+    -pointsize 28 \
+    -draw "translate 0,20 translate 70,430 \
+    text 0,0 '7/1' \
+    text 200,0 '8/1' \
+    text 400,0 '9/1' \
+    text 600,0 '9/30'" \
+\
+    processed/foo.png
 
