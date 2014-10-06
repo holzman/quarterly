84da9df881c5626084d37b5d667cc835ebed4ce3
Burt Holzman <burt@fnal.gov>
2014-10-06 10:27:50 -0500

    Update retrieval scripts to Sep 2014 version

--- getplots.sh
+++ getplots.sh
@@ -1,30 +1,42 @@
-#!/bin/bash 
+#!/bin/bash
 
 credname=x509up_u`id -u`
 credfile=/tmp/${credname}
 
 echo "Edit the getplots.sh script to get the right dates..."
 
-month=09
+month=06
 day=30
-year=2012
+year=2014
 
-begin=2012-07-01
-end=2012-09-30
+begin=2014-04-01
+end=2013-06-30
 
-curl -s -x "" -O http://cms-project-relval.web.cern.ch/cms-project-relval/CustodialSummary/${year}/${month}/${day}/custodial_tape_storage_pie.png
+#curl -s -x "" -O http://cms-project-relval.web.cern.ch/cms-project-relval/CustodialSummary/${year}/${month}/${day}/custodial_tape_storage_pie.png
+curl -s -x "" -O http://transferteam.web.cern.ch/transferteam/StorageOverview/latest/custodial_tape_storage_pie.png
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/FNALViewHistoryParser2.jpg
 curl -s -x "" -o month-ranking-plot.png http://cmsdcam3.fnal.gov:8081/DOEmetrics/TestJobsBars90.png
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/cms_farm_usgae1.jpg
 
 curl -s -x "" -o site-avail.png http://cmsdcam3.fnal.gov:8081/DOEmetrics/ReadinessBars90.png
 
-curl -s -x "" -o utilization_pledged.png "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=All%20T10&activities=all&sitesSort=1&start=${begin}&end=${end}&timeRange=daily&granularity=Daily&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diagT=0&type=up"
+#curl -s -x "" -o processing-share.png "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=T1_CH_CERN&sites=T1_DE_KIT&sites=T1_ES_PIC&sites=T1_FR_CCIN2P3&sites=T1_IT_CNAF&sites=T1_RU_JINR&sites=T1_TW_ASGC&sites=T1_UK_RAL&sites=T1_US_FNAL&sitesSort=1&start=2014-03-01&end=2014-06-30&timeRange=daily&granularity=Weekly&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diag8=0&diagT=0&series=All&type=psa"
 
-curl -s -x "" -o processing-share.png  "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=All%20T10&amp;activities=all&amp;sitesSort=1&amp;start=${begin}&amp;end=${end}&amp;timeRange=daily&amp;granularity=Daily&amp;generic=0&amp;sortBy=0&amp;diag1=0&amp;diag2=0&amp;diag3=0&amp;diag4=0&amp;diag5=0&amp;diag6=0&amp;diag7=0&amp;diagT=0&amp;type=psa"
+#curl -s -x "" -o utilization_pledged.png "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=T1_CH_CERN&sites=T1_DE_KIT&sites=T1_ES_PIC&sites=T1_FR_CCIN2P3&sites=T1_IT_CNAF&sites=T1_RU_JINR&sites=T1_TW_ASGC&sites=T1_UK_RAL&sites=T1_US_FNAL&sitesSort=1&start=2014-03-01&end=2014-06-30&timeRange=daily&granularity=Weekly&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diag8=0&diagT=0&series=All&type=up"
+
+curl -s -x "" -o processing-share.png "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=T1_CH_CERN&sites=T1_DE_KIT&sites=T1_ES_PIC&sites=T1_FR_CCIN2P3&sites=T1_IT_CNAF&sites=T1_RU_JINR&sites=T1_TW_ASGC&sites=T1_UK_RAL&sites=T1_US_FNAL&sitesSort=1&start=2014-03-01&end=2014-06-30&timeRange=daily&granularity=Weekly&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diag8=0&diagT=0&series=All&type=psa"
+curl -s -x "" -o utilization_pledged.png "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=T1_CH_CERN&sites=T1_DE_KIT&sites=T1_ES_PIC&sites=T1_FR_CCIN2P3&sites=T1_IT_CNAF&sites=T1_RU_JINR&sites=T1_TW_ASGC&sites=T1_UK_RAL&sites=T1_US_FNAL&sitesSort=1&start=2014-03-01&end=2014-06-30&timeRange=daily&granularity=Weekly&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diag8=0&diagT=0&series=All&type=up"
+#curl -s -x "" -o processing-share.png "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=T1_CH_CERN&sites=T1_DE_KIT&sites=T1_ES_PIC&sites=T1_FR_CCIN2P3&sites=T1_IT_CNAF&sites=T1_RU_JINR&sites=T1_TW_ASGC&sites=T1_UK_RAL&sites=T1_US_FNAL&sitesSort=1&start=2014-04-01&end=2014-06-30&timeRange=daily&granularity=Weekly&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diag8=0&diagT=0&series=All&type=psa"
+
+#curl -s -x "" -o processing-share.png  "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=All%20T10&sitesSort=1&start=${begin}&end=${end}&timeRange=daily&granularity=Weekly&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diag8=0&diagT=0&series=All&type=psa"
+
+#curl -s -x "" -o processing-share.png  "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=All%20T10&amp;activities=all&amp;sitesSort=1&amp;start=${begin}&amp;end=${end}&amp;timeRange=daily&amp;granularity=Daily&amp;generic=0&amp;sortBy=0&amp;diag1=0&amp;diag2=0&amp;diag3=0&amp;diag4=0&amp;diag5=0&amp;diag6=0&amp;diag7=0&amp;diagT=0&amp;type=psa"
+
+#curl -s -x "" -o utilization_pledged.png "http://dashb-cms-jobsmry.cern.ch/dashboard/request.py/resourceutilization_individual?sites=All%20T10&activities=all&sitesSort=1&start=${begin}&end=${end}&timeRange=daily&granularity=Daily&generic=0&sortBy=0&diag1=0&diag2=0&diag3=0&diag4=0&diag5=0&diag6=0&diag7=0&diagT=0&type=up"
 
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/siteActivity_individual.png
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/cms_dcache_transfers.jpg
+curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/cms_dcachedisk_transfers.jpg
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/diskActivityUsage-t1.jpg
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/diskActivityUsage-t3.jpg
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/cms_farm_usgae.jpg
@@ -32,10 +44,10 @@ curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/PhEDEx-cmlVolumeSource
 curl -s -x "" -O http://cmsdcam3.fnal.gov:8081/DOEmetrics/PhEDEx-cmlVolumeDestinFNAL.png
 
 curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsdcam2.fnal.gov/dcache/CMS_BYTES/cms_bytes2.jpg
-curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsdcam2.fnal.gov/dcache/CMS_BYTES/cms_bytes3.jpg 
-curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsdcam2.fnal.gov/dcache/CMS_BYTES/ALL.jpg 
-curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsjobmon.fnal.gov/RepairHistory-stat-simple.jpg 
-curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsjobmon.fnal.gov/RepairHistory-disk-simple.jpg  
+curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsdcam2.fnal.gov/dcache/CMS_BYTES/cms_bytes3.jpg
+curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsdcam2.fnal.gov/dcache/CMS_BYTES/ALL.jpg
+curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsjobmon.fnal.gov/RepairHistory-stat-simple.jpg
+curl -s -x "" -O -k --key ${credfile} --cert ${credfile} https://cmsjobmon.fnal.gov/RepairHistory-disk-simple.jpg
 
 curl -s -x "" -O -k --key ${credfile} --cert ${credfile} http://cmsdcam3.fnal.gov:8081/tape_verified_delay_plot-90.jpg
 
--- processing.sh
+++ processing.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 arrow_head="path 'M 0,0 l -30,-10 +10,+10 -10,+10 +30,-10 z'"
+big_arrow_head="stroke-width 1 path 'M 0,0 l -60,-20 +20,+20 -20,+20 z'"
 pointer="$arrow_head stroke-width 3 line 0,0, -100,0 "
-xdateaxis="text 0,0 '1/1' text 200,0 '2/1' text 400,0 '3/1' \
-           text 600,0 '3/31'"
+xdateaxis="text 0,0 '7/1' text 200,0 '8/1' text 400,0 '9/1' \
+           text 600,0 '9/30'"
 
+goal="97%"
+act="97.1%"
 # figure 1 -- Overall Hourly Availability
 convert FNALViewHistoryParser2.jpg -pointsize 36 -crop 720x480+0+25 \
-    -draw "text 90, 415 'Goal = 98%, Achieved = 99.2%'" \
+    -draw "text 90, 415 'Goal = ${goal}, Achieved = ${act}'" \
     processed/1__Overall_Hourly_Availability.jpg
+
 
 #figure 2 - performance tests
 cp t1-performance-tests.jpg \
@@ -16,6 +20,7 @@ cp t1-performance-tests.jpg \
 
 # about 60 pixels between vertical columns -- change translate 0,10 to whatever
 
+#    -draw "text 665, 375 'Fail' text 725, 375 'Pass' stroke-width 3 line 715,50 715,385" \
 # figure 3 - site availability
 convert site-avail.png  -pointsize 36 -extent 900x500 \
     -fill white -stroke white \
@@ -28,16 +33,18 @@ convert site-avail.png  -pointsize 36 -extent 900x500 \
 	text 625, 530 '80' \
 	text 745, 530 '100%'" \
     -pointsize 24 \
-    -draw "text 665, 375 'Fail' text 725, 375 'Pass' stroke-width 3 line 715,50 715,385" \
     -pointsize 36 \
     -draw "\
-	scale .975,.75 translate 0,10
+	scale .975,.75 translate 0,180 \
 	stroke-width 1 fill yellow stroke yellow \
 	text 400, 95 'FNAL' \
+	stroke-width 1
+	translate 800,85 $big_arrow_head
 	stroke-width 10
-	translate 760,85 $arrow_head
+        translate -40,0
 	line 0,0 -240,0 line -660,0 -380,0
-	rotate 180 translate 660,0 $arrow_head" \
+	stroke-width 1
+	rotate 180 translate 680,0 $big_arrow_head" \
   processed/3__Site_Availability.png
 
 # figure 4 - job robot rank
@@ -55,71 +62,91 @@ convert month-ranking-plot.png  -pointsize 36 -extent 900x500 \
     -draw "text 665, 375 'Fail' text 725, 375 'Pass' stroke-width 3 line 715,50 715,385" \
     -pointsize 36 \
     -draw "\
-	scale .975,.75 translate 0,10
+	scale .975,.75 translate 0,238 \
 	stroke-width 1 fill yellow stroke yellow \
 	text 400, 95 'FNAL' \
+	translate 800,85 $big_arrow_head
 	stroke-width 10
-	translate 760,85 $arrow_head
+        translate -40,0
 	line 0,0 -240,0 line -660,0 -380,0
-	rotate 180 translate 660,0 $arrow_head" \
+	rotate 180 translate 680,0 $big_arrow_head" \
   processed/4__Job_Robot_Rank.png
 
-# figure 5 - tape latency plots from CERN
-convert -extent 720x604+0-100 tape_verified_delay_plot-90.jpg \
-    -fill white -stroke white \
-    -draw "rectangle 70,101 700,126" \
-    -fill white -stroke white \
-    -draw "rectangle 200,140 700,191" \
-    -fill black -stroke black \
-    -pointsize 32 \
-    -draw " \
-	text 220, 70 'Time to transfer files:' \
-	text 240, 120 'Available at CERN' \
-	text 200, 165 'to being on tape at FNAL'" \
-    -strokewidth 1 \
-    -fill red -stroke red \
-    -draw "translate 0,320 stroke-width 3 line 70,0 700,0 stroke-width 1 text 100,-10 '12 Hour Goal'" \
-    processed/5__Tape_Latency.png
-
+# figure 5 - tape latency plot
+cp tape_delay_plot.jpg processed/5__Tape_Latency.jpg
+# # figure 5 - tape latency plots from CERN
+# convert -extent 720x604 -gravity south tape_verified_delay_plot-90.jpg \
+#     -gravity northwest -fill white -stroke white \
+#     -draw "rectangle 70,101 700,126" \
+#     -fill white -stroke white \
+#     -draw "rectangle 200,140 700,191" \
+#     -fill black -stroke black \
+#     -pointsize 32 \
+#     -draw "translate 200, 50 \
+# 	text 20, 0 'Time to transfer files:' \
+# 	text 40, 50 'Available at CERN' \
+# 	text 0, 95 'to being on tape at FNAL'" \
+#     -strokewidth 1 \
+#     -fill red -stroke red \
+#     -draw "translate 0,320 stroke-width 3 line 70,0 700,0 stroke-width 1 text 100,-40 '12 Hour Goal'" \
+#     processed/5__Tape_Latency.png
 
 # figure 6 - computing utilization
 convert cms_farm_usgae.jpg \
     -fill lightblue -stroke lightblue \
     -pointsize 18 \
-    -draw "translate 150, 100 text 0,0 'LPC Analysis' text 30, 20 'Jobs'" \
+    -draw "translate 250, 160 text 0,0 'LPC Analysis' text 30, 20 'Jobs'" \
     processed/6__Computing_Utilization.png
 
 # figure 7 - queued jobs
 cp cms_farm_usgae1.jpg \
     processed/7__Queued_Jobs.jpg
 
+col8a=89000f
+
 # figure 8a - processing share
 convert processing-share.png \
-    -fill \#f91b85 -stroke \#f91b85 \
+    -fill \#${col8a} -stroke \#${col8a} \
     -pointsize 24 \
-    -draw "translate 645,85 text 0, 0 'FNAL' translate -80,70 rotate 135 $pointer " \
+    -draw "translate 430,115 text 0, 0 'FNAL' translate -80,70 rotate 135 $pointer " \
     processed/8A__Processing_Share.png
 
 #    -draw "translate 325,235 text -65, 10 'FNAL' translate -80,170 rotate 105 scale 1.5,1 $pointer " \
 # figure 8b - percent utilization wrt share
+col8b=3c9623
+# convert utilization_pledged.png \
+#     -fill \#${col8b} -stroke \#${col8b} \
+#     -pointsize 18 \
+#     -draw "translate 710,280 text -65, 10 'FNAL' translate -80,170 rotate 105 scale 1.5,1 $pointer " \
+#     -strokewidth 3 \
+#     -fill red -stroke red -strokewidth 5\
+#     -draw "translate 0, 435 line 100,0 718,0" \
+#     -strokewidth 1 \
+#     -draw "translate 430,350 text -90, -155 '100% utilization' translate -80,70 rotate 105 scale 2,1 $pointer " \
+# 	processed/8B__Utilization_Pledged.png
+
 convert utilization_pledged.png \
-    -fill \#679819 -stroke \#679819 \
+    -fill \#${col8b} -stroke \#${col8b} \
     -pointsize 18 \
-    -draw "translate 670,260 text -65, 10 'FNAL' translate -80,170 rotate 105 scale 1.5,1 $pointer " \
     -strokewidth 3 \
     -fill red -stroke red -strokewidth 5\
-    -draw "translate 0, 470 line 100,0 718,0" \
+    -draw "translate 0, 379 line 100,0 718,0" \
     -strokewidth 1 \
-    -draw "translate 500,385 text -90, -115 '100% utilization' translate -80,70 rotate 105 scale 1.75,1 $pointer " \
+    -draw "translate 430,300 text -90, -155 '100% utilization' translate -80,70 rotate 105 scale 2,1 $pointer " \
 	processed/8B__Utilization_Pledged.png
+
 
 # figure 9 - successful and failed jobs
 cp siteActivity_individual.png \
     processed/9__Success_Failed_jobs.png
 
-#figure 10 - dcache data rates
+#figure 10A - dcache data rates
 cp cms_dcache_transfers.jpg \
-    processed/10__Dcache_Data_Rates.jpg
+    processed/10A__Dcache_Data_Rates.jpg
+
+#figure 10 - dcache data rates
+cp cms_dcachedisk_transfers.jpg \
+    processed/10B__DcacheDisk_Data_Rates.jpg
 
 #figure 11 - bytes on tape
 convert -extent 740x504 -crop 740x480+0-24 cms_bytes2.jpg \
@@ -130,13 +157,16 @@ convert -extent 740x504 -crop 740x480+0-24 cms_bytes2.jpg \
     -draw "translate 70,430 $xdateaxis" \
 \
     -pointsize 24 \
-    -draw "fill yellow stroke white text 200, 160 'Deleted/Squeezable'" \
-    -draw "fill yellow stroke yellow text 200, 270 'Monte Carlo'" \
-    -draw "text 200, 350 'Custodial Data'" \
+    -draw "fill yellow stroke white text 200, 190 'Deleted/Squeezable'" \
+    -draw "fill yellow stroke yellow text 200, 280 'Monte Carlo'" \
+    -draw "text 200, 360 'Custodial Data'" \
     -pointsize 18 \
-    -draw "text 200, 392 ' Legacy'" \
-    -draw "fill magenta stroke magenta translate 500,240 text 0, 0 'Users'  scale .5,.5 translate 190,-80 rotate -30 $pointer " \
+    -draw "fill magenta stroke magenta translate 450,280 text 0, 0 'Users'  scale .5,.5 translate 190,-80 rotate -30 $pointer " \
     processed/11__Bytes_On_Tape.png
+
+
+#    -draw "text 200, 392 ' Legacy'" \
+
 
 # figure 12 - bytes on tape delta
 convert -extent 740x504 -crop 740x480+0-24 cms_bytes3.jpg \
@@ -145,7 +175,7 @@ convert -extent 740x504 -crop 740x480+0-24 cms_bytes3.jpg \
     -fill black -stroke black \
     -pointsize 28 \
     -draw "translate 0,20 translate 70,430 $xdateaxis" \
-    -draw "translate 300,100 text 0, 0 'Additions'  scale 1,.75 translate 60, 20 rotate -90 " \
+    -draw "translate 300,70 text 0, 0 'Additions'  scale 1,.75 translate 60, 20 rotate -90 " \
     -draw "translate 300,400 text 0, 0 'Deletions'  scale 1,.75 translate 60, -40 rotate 90 " \
     processed/12__Bytes_On_Tape_Delta.png
 
@@ -166,20 +196,17 @@ convert ALL.jpg \
     -draw "rectangle 0,0 740,30" \
 \
     -fill black -stroke black \
-    -draw "translate 10,85 \
-	text 0,0 '11 PB' \
-	text 0,58 '10 PB' \
-	text 0,116 '9 PB' \
-	text 0,174 '8 PB' \
-	text 0,232 '7 PB' \
-	text 0,290 '6 PB' \
-        text 0,350 '5 PB' \
+    -draw "translate 5,87 \
+	text 0,45 '20 PB' \
+	text 0,130 '15 PB' \
+        text 0,215 '10 PB' \
+        text 0,300 '5 PB' \
 	" \
     -draw "translate 00,70 translate 70,430 \
     text 0,0 \"Sep '09\" \
-    text 200,0 \"Sep '10\" \
-    text 400,0 \"Sep '11\" \
-    text 550,0 \"Mar '12\" " \
+    text 200,0 \"Sep '11\" \
+    text 400,0 \"Sep '13\" \
+    text 550,0 \"Nov '14\" " \
 \
     -draw "text 350, 80 'Bytes on Tape'" \
     processed/13__Bytes_On_Tape_Since_LHC_Start.png
@@ -196,8 +223,9 @@ convert diskActivityUsage-t3.jpg \
 convert PhEDEx-cmlVolumeDestinFNAL.png \
     -pointsize 24 \
     -draw "translate 100, 100 text 0,0 'Data delivered from CMS to FNAL'" \
-    -draw "translate 500, 200 text 0,0 'T0-CERN'" \
     processed/15__Phedex_to_FNAL.png
+
+#    -draw "translate 500, 170 text 0,0 'T0-CERN'" \
 
 # figure 16 - data from FNAL (PhEDEx)
 convert PhEDEx-cmlVolumeSourceFNAL.png \
